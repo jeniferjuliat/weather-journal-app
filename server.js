d6673f8a7088883120f3018da2909174b523acb7
Jenifer Juliat <107074702+jeniferjuliat@users.noreply.github.com>
2022-11-25 13:24:05 -0300

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ const express = require('express');
 const app = express();
 
 /* Dependencies */
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+
 
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -22,19 +23,65 @@ app.use(cors());
 app.use(express.static('website'));
 
 //Post Route
-app.post('/all', (req, res) => {
+app.post('/add', (req, res) => {
     newEntry = {
     date: req.body.date,
     temp: req.body.temp,
     content: req.body.content
    }
-   projectData.push(newEntry)
-   res.send(projectData)
+   projectData = newEntry;
   });
 
 
 // Callback function to complete GET '/all'
-app.get('/all', (req,res) => {
+app.get('all', (req,res) => {
+  res.send(projectData);
+  });
+
+
+// Setup Server
+const port = 8000;
+const server = app.listen(port, () =>
+{console.log(`running on localhost: ${port}`)
+});
+
+// Setup empty JS object to act as endpoint for all routes
+let projectData = {};
+
+// Express to run server and routes
+const express = require('express');
+
+// Start up an instance of app
+const app = express();
+
+/* Dependencies */
+const bodyParser = require('body-parser');
+
+
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
+
+// Initialize the main project folder
+app.use(express.static('website'));
+
+//Post Route
+app.post('/add', (req, res) => {
+    newEntry = {
+    date: req.body.date,
+    temp: req.body.temp,
+    content: req.body.content
+   }
+   projectData = newEntry;
+  });
+
+
+// Callback function to complete GET '/all'
+app.get('all', (req,res) => {
   res.send(projectData);
   });
 
