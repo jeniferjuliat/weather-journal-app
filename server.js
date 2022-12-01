afb4548f45e88875245438808b1c0ef8eadc3b10
Jenifer Juliat <107074702+jeniferjuliat@users.noreply.github.com>
2022-11-30 21:44:37 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // Setup empty JS object to act as endpoint for all routes
-let projectData = {};
+projectData = {};
 
 // Express to run server and routes
 const express = require('express');
@@ -22,73 +22,34 @@ app.use(cors());
 // Initialize the main project folder
 app.use(express.static('website'));
 
-//Post Route
-app.post('/add', (req, res) => {
-    newEntry = {
-    date: req.body.date,
-    temp: req.body.temp,
-    content: req.body.content
-   }
-   projectData = newEntry;
-  });
-
-
-// Callback function to complete GET '/all'
-app.get('all', (req,res) => {
-  res.send(projectData);
-  });
-
-
 // Setup Server
 const port = 8000;
-const server = app.listen(port, () =>
-{console.log(`running on localhost: ${port}`)
-});
+const server = app.listen(port, listening);
+ function listening(){
+    console.log(`running on localhost: ${port}`);
+  };
 
-// Setup empty JS object to act as endpoint for all routes
-let projectData = {};
-
-// Express to run server and routes
-const express = require('express');
-
-// Start up an instance of app
-const app = express();
-
-/* Dependencies */
-const bodyParser = require('body-parser');
-
-
-//Here we are configuring express to use body-parser as middle-ware.
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-// Cors for cross origin allowance
-const cors = require('cors');
-app.use(cors());
-
-// Initialize the main project folder
-app.use(express.static('website'));
+  // Callback function to complete GET '/all'
+app.get('/all', sendData);
+function sendData (request, response) {
+  response.send(projectData);
+};
 
 //Post Route
-app.post('/add', (req, res) => {
-    newEntry = {
-    date: req.body.date,
-    temp: req.body.temp,
-    content: req.body.content
-   }
-   projectData = newEntry;
-  });
+app.post('/add', callBack);
+function callBack(req,res){
+  res.send('POST received');
+}
+
+const data = [];
+
+app.post('/weather', addWeather);
+
+function addWeather (req,res){
+    data.push(req.body);
+};
 
 
-// Callback function to complete GET '/all'
-app.get('all', (req,res) => {
-  res.send(projectData);
-  });
 
 
-// Setup Server
-const port = 8000;
-const server = app.listen(port, () =>
-{console.log(`running on localhost: ${port}`)
-});
 
