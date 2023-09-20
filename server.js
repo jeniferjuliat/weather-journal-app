cfea46496f94e4a736ac8aae1f8986901321a366
Jenifer Juliat <107074702+jeniferjuliat@users.noreply.github.com>
2023-09-20 09:31:45 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,32 +1,51 @@
-let projectData = {};
+// Setup empty JS object to act as endpoint for all routes
+projectData = {};
 
+// Express to run server and routes
 const express = require('express');
 
+// Start up an instance of app
 const app = express();
-const port = 8000;
 
-app.use(express.json());
+/* Dependencies */
+const bodyParser = require('body-parser');
 
-app.use(express.urlencoded({extended: false}));
 
-app.use(express.static("website"));
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
+// Cors for cross origin allowance
 const cors = require('cors');
-
 app.use(cors());
 
+// Initialize the main project folder
+app.use(express.static('website'));
+
+// Setup Server
+const port = 8008;
+
+// POST 
 app.post("/add", async function(req, res){
-    const info = await req.body;
-    projectData = info;
-    res.send(projectData);
+  const info = await req.body;
+  projectData = info;
+  res.send(projectData);
 });
 
-app.get("/all", async (req, res) => {
+
+  // GET 
+  app.get("/all", async (req, res) => {
     if(projectData){
     res.send(projectData);
     }
 });
 
-app.listen(port, function(){console.log("listening on port " + port)});
 
-                           
+// server setup
+const server = app.listen(port, listening);
+ function listening(){
+    console.log(`running on localhost: ${port}`);
+  };
+
+
+
