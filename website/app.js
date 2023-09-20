5e63af43952d1557d288fb552e1dc6c73db32ed3
Jenifer Juliat <107074702+jeniferjuliat@users.noreply.github.com>
2023-09-20 09:38:34 -0300

    Update app.js

--- website/app.js
+++ website/app.js
@@ -1,106 +1,77 @@
-const generate = document.querySelector("#generate");
-const zip = document.querySelector("#zip");
-const feelings = document.querySelector("#feelings");
-const feeling = document.querySelector(".feeling");
-const temp = document.querySelector("#temp");
-const dateNow = document.querySelector('#dateNow')
+// Initialize variables
 const d = new Date();
 const date = d.toDateString();
 
-const baseURL = 'https://api.openweathermap.org/data/2.5/weather?zip='
+const baseURL = 'https://api.openweathermap.org/data/2.5/weather?zip=';
 const apiKey = '&appid=cbcc3079d870a8e1433045013f9bbe87&units=imperial';
 
-generate.addEventListener("click", (event) => {
+// Event listener for 'Enter' key press on feelings input
+document.querySelector("#feelings").addEventListener("keydown", function(event) {
+  if (event.key === "Enter") {
     event.preventDefault();
-    const madeURI = `${baseURL}${zip.value}${apiKey}`;
-    getData(madeURI)
-    .then((data) => {
-        cureData(data)
-        .then((info) => {
-            postData("/add", info)
-            .then((data) => {
-                retreiveData("/all")
-                .then(data => {
-                    updateUI(data);
-                })
-            });
-        });
-    });
+    document.querySelector("#generate").click();
+  }
 });
 
-const getData = async (url) => {
-    try{
-        const result = await fetch(url);
-        const data = await result.json();
-        if(data.cod == 200){
-            return data; 
-        } else {
-            console.log(data.message);
-            return data;
-        }
-    }catch(e){
-        console.log(e);
-    }
-};
+// Event listener for "Generate" button click
+document.querySelector("#generate").addEventListener("click", async (event) => {
+  event.preventDefault();
+  const zip = document.querySelector("#zip").value;
+  const feelings = document.querySelector("#feelings").value;
 
-const cureData = async (data)=>{
-    try{
-        if(data.message){
-            return data;
-        } else {
-        const info = {
-            date,
-            feelings: feelings.value,
-            temp: data.main.temp
-        }; 
-        return info;
+  try {
+    // Fetch weather data
+    const weatherData = await getData(`${baseURL}${zip}${apiKey}`);
+
+    // Prepare and post data
+    if (weatherData.cod === 200) {
+      const postData = {
+        date,
+        placeName: weatherData.name,
+        feelings,
+        description: weatherData.weather[0].description,
+        temp: weatherData.main.temp,
+      };
+      
+      await fetch("/add", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(postData),
+      });
+
+      // Fetch and update UI
+      const allData = await fetch("/all").then((res) => res.json());
+      updateUI(allData);
+    } else {
+      console.error(weatherData.message);
     }
-} catch(err){
+  } catch (err) {
     console.error(err);
-}
+  }
+});
+
+// Function to fetch data from API
+const getData = async (url) => {
+  const response = await fetch(url);
+  return response.json();
 };
 
-// POST
-const postData  = async (url="", data={}) => {
-        const result = await fetch (url, {
-            method: "POST",
-            credentials: "same-origin",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(data),
-        });
-        try{
-            const response = await result.json();
-            return response;
-        } catch(err){
-            console.error(err);
-        }
-    };
+// Function to update UI
+const updateUI = (data) => {
+  document.querySelector("#dateNow").textContent = data.date;
+  document.querySelector("#temp").textContent = `${data.temp}°F`;
+  document.querySelector("#content").textContent = data.feelings || "What do you feel?";
+  document.querySelector("#placeName").textContent = data.placeName;
+  document.querySelector("#description").textContent = 
+    data.description.charAt(0).toUpperCase() + data.description.slice(1);
 
-// GET
-
-const retreiveData = async (url) => {
-    const data = await fetch(url);
-    try {
-        const response = await data.json();
-        return response;
-    } catch(err){
-        console.error(err);
-    }
-};
-
-const updateUI = async (data) => {
-   const response = await data;
-   if(response.date){
-    document.querySelector(".outputs").style.display = "block";
-    dateNow.innerHTML = response.date;
-    temp.innerHTML = response.temp;
-    feeling.innerHTML = response.feelings ? response.feelings: "What do you feel?";
-    document.querySelector("#error").style.display = "none";
-   }else{
-    document.querySelector(".outputs").style.display = "none";
-    document.querySelector("#error").style.display = "block"; 
-    document.querySelector("#error").innerHTML = response.message;
-   }
+  // Change background image based on temperature
+  const body = document.body;
+  if (data.temp < 60) {
+    body.style.backgroundImage = "url('img/cold.jpg')";
+  } else if (data.temp >= 60 && data.temp < 80) {
+    body.style.backgroundImage = "url('img/medium.jpg')";
+  } else {
+    body.style.backgroundImage = "url('img/hot.jpg')";
+  }
 };
